0753b574c2d4fcf65e09331914b45ec85f11295d
arun raj peddhala <arunrajaru325@gmail.com>
2024-06-18 22:51:32 -0400

    Initial commit

---
+++ Controllers/OperationsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Calculator.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Calculator.Controllers
+{
+    public class OperationsController : Controller
+    {
+        OperationModel? operationModel;
+        public IActionResult operationAdd()
+        {
+            OperationModel o = new OperationModel();
+            o.x = 22;
+            o.y = 33;
+            o.z = o.x + o.y;
+            return View(o);
+        }
+        //get
+        [HttpGet]
+
+        public IActionResult OperationAll()
+        {
+            operationModel = new OperationModel();
+            return View(operationModel);
+
+        }
+
+        //post method
+        [HttpPost]
+
+        public IActionResult OperationAll(OperationModel obj)
+        {
+            operationModel = new OperationModel();
+            operationModel.x = obj.x;
+            operationModel.y = obj.y;
+            operationModel.operation = obj.operation;
+            switch (operationModel.operation)
+            {
+                case "add":
+                    operationModel.Addition();
+                    break;
+                case "subtract":
+                    operationModel.Subtract();
+                    break;
+                case "multiply":
+                    operationModel.Multiply();
+                    break;
+                case "divide":
+                    operationModel.Division();
+                    break;
+
+            }
+
+            return View(operationModel);
+        }
+    }
+}
---
+++ Models/OperationModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Calculator.Models
+{
+    public class OperationModel
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+        public string operation { get; set; }
+
+        public void Addition()
+        {
+            z = x + y;
+        }
+
+        public void Subtract()
+        {
+            z = x - y;
+        }
+
+        public void Multiply()
+        {
+            z = x * y;
+        }
+
+        public void Division()
+        {
+            if (y != 0)
+            {
+                z = x / y;
+
+            }
+        
+        }
+    }
+}
+
